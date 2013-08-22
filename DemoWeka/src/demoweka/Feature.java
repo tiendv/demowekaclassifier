c846b3b8e89c53c9d3a01c1a2cc091b4261a4568
tiendo.vn <tiendo.vn@2158146b-1a2d-8534-c23d-7514b9cd6c3d>
2013-08-22 15:49:37 +0000

    First commit

---
+++ DemoWeka/src/demoweka/Feature.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoweka;
+
+/**
+ *
+ * @author tiendv
+ */
+public class Feature {
+    String name;
+    int Value;  
+}
