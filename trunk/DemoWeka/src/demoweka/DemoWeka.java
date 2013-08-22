7f71241d75b1c848a2867990f6358de02343c6a1
tiendo.vn <tiendo.vn@2158146b-1a2d-8534-c23d-7514b9cd6c3d>
2013-08-22 16:23:20 +0000

    update main

--- trunk/DemoWeka/src/demoweka/DemoWeka.java
+++ trunk/DemoWeka/src/demoweka/DemoWeka.java
@@ -3,6 +3,10 @@
  * and open the template in the editor.
  */
 package demoweka;
+ import weka.classifiers.Classifier;
+ import weka.core.Instances;
+ import weka.classifiers.Evaluation;
+ import weka.classifiers.trees.J48;
 
 /**
  *
@@ -13,7 +17,19 @@ public class DemoWeka {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
         // TODO code application logic here
+        Instances train = BuildVector.buildVector(null, 100); // Dữ liệu train
+        Instances test =  BuildVector.buildVector(null, 50); // Dữ liệu Test
+        // train classifier Gọi thuật toán phân lớp 
+        
+        Classifier cls = new J48(); //Gọi thuật toán phân lớp 
+        cls.buildClassifier(train); //Build model đối với dữ liệu train
+        
+        // evaluate classifier and print some statistics
+        Evaluation eval = new Evaluation(train); //Chạy đánh giá model xây dựng
+        eval.evaluateModel(cls, test);
+        System.out.println(eval.toSummaryString("\nResults\n======\n", false));
     }
 }
